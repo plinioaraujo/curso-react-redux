c67e1b64a6a0a102205c0f695d286b36b7c5a886
Plinio Araujo <plinioglad@gmail.com>
2022-01-02 14:31:22 -0300

    Curso React + redux - Exibindo String na tela - ReactDom

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 ##Criando o projeto React JS 
 
 # curso-react-redux
-# curso-react-redux
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+
+const el = document.getElementById('root'); 
+
+ReactDom.render("Olá React!!!!",el);
